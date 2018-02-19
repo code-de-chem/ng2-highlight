338b5f0f13b948730c30758c08e3e32568c24e42
code-de-chem <saurabhsingh368@gmail.com>
2018-02-20 02:55:07 +0530

    pre publish

--- src/app/highlight.module.ts
+++ src/app/highlight.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule
   ],
-  declarations: [HighlightDirective],
-  exports: [HighlightDirective]
+  declarations: [
+    HighlightDirective
+  ],
+  exports: [
+    HighlightDirective
+  ],
+  providers: [],
+  bootstrap: []
 })
 export class HighlightModule { }
